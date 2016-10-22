b4bdaaf11b3498612eaa26374aa38cd91222950e
chris <chris>
2016-10-22 12:08:30 -0500

    Added some ps scripts, work in progress

--- ad/setupDC.ps1
+++ ad/setupDC.ps1
@@ -2,13 +2,16 @@
 # Windows PowerShell script for AD DS Deployment
 #
 
+$domain = "diehlabs.tech"
+$netbios = $domain.Split(".")[0].ToUpper()
+
 Import-Module ADDSDeployment
 Install-ADDSForest `
 -CreateDnsDelegation:$false `
 -DatabasePath "C:\Windows\NTDS" `
 -DomainMode "Win2012R2" `
--DomainName "diehlabs.tech" `
--DomainNetbiosName "DIEHLABS" `
+-DomainName $domain `
+-DomainNetbiosName $netbios `
 -ForestMode "Win2012R2" `
 -InstallDns:$true `
 -LogPath "C:\Windows\NTDS" `
---
+++ dsc/CreatePullServer.ps1
@@ -0,0 +1,49 @@
+﻿configuration HTTPPullServer
+{
+    # Modules must exist on target pull server
+    Import-DSCResource -ModuleName PSDesiredStateConfiguration
+    Import-DscResource -ModuleName xDscWebService
+
+    Node 192.168.1.100
+    {
+        WindowsFeature DSCServiceFeature
+        {
+            Ensure = "Present"
+            Name   = "DSC-Service"
+        }
+
+        WindowsFeature IISConsole {
+            Ensure = "Present"
+            Name   = "Web-Mgmt-Console"
+        }
+
+        xDscWebService PSDSCPullServer
+        {
+            Ensure                  = "Present"
+            EndpointName            = "PSDSCPullServer"
+            Port                    = 8080
+            PhysicalPath            = "$env:SystemDrive\inetpub\wwwroot\PSDSCPullServer"
+            CertificateThumbPrint   = "AllowUnencryptedTraffic"
+            ModulePath              = "$env:PROGRAMFILES\WindowsPowerShell\DscService\Modules"
+            ConfigurationPath       = "$env:PROGRAMFILES\WindowsPowerShell\DscService\Configuration"
+            State                   = "Started"
+            DependsOn               = "[WindowsFeature]DSCServiceFeature"
+        }
+
+        xDscWebService PSDSCComplianceServer
+        {
+            Ensure                  = "Present"
+            EndpointName            = "PSDSCComplianceServer"
+            Port                    = 9080
+            PhysicalPath            = "$env:SystemDrive\inetpub\wwwroot\PSDSCComplianceServer"
+            CertificateThumbPrint   = "AllowUnencryptedTraffic"
+            State                   = "Started"
+            #IsComplianceServer      = $true
+            DependsOn               = ("[WindowsFeature]DSCServiceFeature","[xDSCWebService]PSDSCPullServer")
+        }
+    }
+}
+
+HTTPPullServer -OutputPath c:\temp\http
+
+Start-DscConfiguration -Path C:\temp\http -ComputerName 192.168.1.100 -Verbose -Wait
